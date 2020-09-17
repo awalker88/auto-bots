25d803b1b36c2e3979c6991cb5f41bf9e459566b
AndrewWalker <andrew.walker@ibm.com>
2020-09-17 09:15:16 -0500

    add mase error function

--- error_metrics.py
+++ error_metrics.py
@@ -1,18 +1,29 @@
-def mse():
+import pandas as pd
+
+
+def mse(data: pd.DataFrame, prediction: str, actuals: str):
     pass
 
 
-def rmse():
+def rmse(data: pd.DataFrame, prediction: str, actuals: str):
     pass
 
 
-def mape(x, y):
+def mape(data: pd.DataFrame, prediction: str, actuals: str):
     pass
 
 
-def smape(x, y):
+def smape(data: pd.DataFrame, prediction: str, actuals: str):
     pass
 
 
-def mase(x, y):
-    pass
+def mase(data: pd.DataFrame, prediction: str, actuals: str):
+    data = data[[prediction, actuals]].copy()
+    data['shifted_actuals'] = data[actuals].shift(1)
+    data['abs_shifted_error'] = abs(data[actuals] - data['shifted_actuals'])
+    data['abs_prediction_error'] = abs(data[actuals] - data[prediction])
+
+    avg_shifted_error = data[~data['abs_shifted_error'].isna()]['abs_shifted_error'].sum() / (len(data) - 1)
+    data['abs_scaled_error'] = data['abs_prediction_error'] / avg_shifted_error
+
+    return data['abs_scaled_error'].mean()
