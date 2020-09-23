63332cf4802c38dc6abaf292987a597d3da11e9a
AndrewWalker <andrew.walker@ibm.com>
2020-09-23 11:11:10 -0500

    initial add_zero_rows function

--- AutoTS.py
+++ AutoTS.py
@@ -1,59 +1,85 @@
+import warnings
+from math import floor
+
 import pandas as pd
 from pmdarima import auto_arima
 from numpy import inf
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from error_metrics import mase
+from fbprophet import Prophet
 
-import warnings
+from validate_inputs import validate_inputs
 
+
+# todo: have user be able to access all models in addition to the one that works best
+# todo: more basic models as options (ma, run_rate)
+# todo: make example notebook
+# todo: add dlm model
 
 class AutoTS:
     def __init__(self,
-                 models=('auto_armia', 'exponential_smoothing', 'TBATS', 'prophet', 'ensemble'),
+                 models=('auto_arima', 'exponential_smoothing', 'tbats', 'prophet', 'ensemble'),
                  model_args: dict = None,
-                 # error_metric: str = 'mase',
+                 error_metric: str = 'mase',
                  is_seasonal: bool = True,
-                 seasonal_period: int = 4,
-                 holdout_length: int = 4
+                 seasonal_period: int = 3,
+                 seasonality_mode: str = 'm',
+                 holdout_period: int = 3
                  ):
 
-        if type(models) not in [tuple, list]:
-            raise TypeError('`models` argument must a list or tuple')
+        validate_inputs(models)
 
         self.models = [model.lower() for model in models]
         self.model_args = model_args
-        # self.error_metric = error_metric.lower()
+        self.error_metric = error_metric.lower()
         self.is_seasonal = is_seasonal
         self.seasonal_period = seasonal_period
-        self.holdout_length = holdout_length
+        self.holdout_period = holdout_period
 
+        # Set during fitting or by other methods
         self.data = None
-        self.x = None
+        self.training_data = None
+        self.testing_data = None
+        self.series_column_name = None
         self.exogenous = None
         self.using_exogenous = False
 
         warnings.filterwarnings('ignore', module='statsmodels')
 
-    def fit(self, data: pd.DataFrame, x: str, exogenous: list = None):
-        self.data = data
-        self.x = x
+    def fit(self, data: pd.DataFrame, series_column_name: str, exogenous: list = None):
+        self._set_input_data(data, series_column_name)
 
         if exogenous is not None:
             self.using_exogenous = True
             self.exogenous = exogenous
 
-        arima = self._fit_auto_arima()
-        es = self._fit_exponential_smoothing()
+        arima, arima_error = self._fit_auto_arima()
+        es, es_error = self._fit_exponential_smoothing()
+        print(arima_error, es_error)
+
+        return None
 
     def _fit_auto_arima(self):
-        exog = None
+        train_exog = None
+        test_exog = None
         if self.using_exogenous:
-            exog = self.data[self.exogenous]
-        arima = auto_arima(self.data[self.x], exogenous=exog, error_action='ignore',
-                           supress_warning=True, seasonal=self.is_seasonal, m=self.seasonal_period)
-        return arima
+            train_exog = self.training_data[self.exogenous]
+            test_exog = self.testing_data[self.exogenous]
 
-    def _fit_exponential_smoothing(self, hypertune: bool = True):
+        model = auto_arima(self.training_data[self.series_column_name], exogenous=train_exog,
+                           error_action='ignore',
+                           supress_warning=True,
+                           seasonal=self.is_seasonal, m=self.seasonal_period
+                           )
+
+        predictions = pd.DataFrame({'actuals': self.testing_data[self.series_column_name],
+                                    'predictions': model.predict(n_periods=len(self.testing_data), exogenous=test_exog)})
+
+        error = self._error_metric(predictions, 'predictions', 'actuals')
+
+        return model, error
+
+    def _fit_exponential_smoothing(self, hypertune: bool = False):
         params = {
             'trend': ['add', 'mul'],
             'is_seasonal': ['add', 'mul'],
@@ -64,40 +90,66 @@ class AutoTS:
             best_model = None
             for trend in params['trend']:
                 for seasonal in params['is_seasonal']:
-                    es_model = ExponentialSmoothing(temp_df[self.x],
+                    es_model = ExponentialSmoothing(temp_df[self.series_column_name],
                                                     seasonal_periods=self.seasonal_period,
                                                     trend=trend,
                                                     seasonal=seasonal,
-                                                    use_boxcox=True,
-                                                    initialization_method="estimated").fit()
+                                                    use_boxcox=False,
+                                                    initialization_method='estimated').fit()
                     predictions = es_model.predict(start=0, end=len(temp_df) + 1)
                     temp_df['prediction'] = predictions
-                    error = mase(temp_df, 'prediction', 'expense_plan_amount')
+                    error = self._error_metric(temp_df, 'prediction', 'expense_plan_amount')
                     if error < lowest_error:
                         lowest_error = error
                         best_model = es_model
             return best_model
 
-        es_model = ExponentialSmoothing(self.data[self.x],
-                                        seasonal_periods=self.seasonal_period,
-                                        trend='add',
-                                        seasonal='add',
-                                        use_boxcox=True,
-                                        initialization_method="estimated").fit()
+        best_model = ExponentialSmoothing(self.data[self.series_column_name],
+                                          seasonal_periods=self.seasonal_period,
+                                          trend='add',
+                                          seasonal='add',
+                                          use_boxcox=False,
+                                          initialization_method='estimated').fit()
 
-        return es_model
+        predictions = pd.DataFrame({'actuals': self.data[self.series_column_name],
+                                    'predictions': best_model.predict(0)})
+        error = self._error_metric(predictions, 'predictions', 'actuals')
+
+        return best_model, error
 
     def _fit_tbats(self):
         pass
 
     def _fit_prophet(self):
-        pass
+
+        # have to check if the dataframe already contains
+        datetime_index_name = self.data.index.name
+        if datetime_index_name in self.data.columns:
+            proph_df = self.data.reset_index(drop=True)
+        else:
+            proph_df = self.data.reset_index()
+
+        # prophet requires the input dataframe to have two columns named 'ds' and 'y'
+        proph_df = proph_df[[datetime_index_name, self.series_column_name]].\
+            rename({datetime_index_name: 'ds', self.series_column_name: 'y'}, axis='columns')
+
+        model = Prophet(changepoint_range=1.,  # set to 1 since we're fitting only on training data
+                        )
+
+        return Prophet().fit(proph_df)
 
     def _fit_ensemble(self):
         pass
 
-    def _error_metric(self):
-        pass
+    def _error_metric(self, data: pd.DataFrame, predictions_column: str, actuals_column: str):
+        if self.error_metric == 'mase':
+            return mase(data, predictions_column, actuals_column)
+
+    def _set_input_data(self, data: pd.DataFrame, series_column_name: str):
+        self.data = data
+        self.training_data = data.iloc[:-self.holdout_period, :]
+        self.testing_data = data.iloc[-self.holdout_period:, :]
+        self.series_column_name = series_column_name
 
     def predict(self):
         pass
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pickle as pkl
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from AutoTS import AutoTS
 
@@ -8,19 +7,59 @@ from time import time
 
 
 def main():
-    split_dfs = pkl.load(open('split_dfs.pkl', 'rb'))
-
-    test_df: pd.DataFrame = split_dfs[0]
-    test_df = test_df.set_index('finance_start_date', drop=False)
-
+    split_dfs = clean_data()
+    config = {'prediction_splitters': ['geography_code', 'market_name', 'dual_unit_name', 'dual_sub_unit_name']}
     start_time = time()
 
-    model = AutoTS()
-
-    model = model.fit(test_df, x='expense_plan_amount', exogenous=['is_covid'])
+    for i, tdf in enumerate(split_dfs):
+        atomic_id = ', '.join([f'{splitter}: {str(tdf[splitter].iloc[0])}'
+                               for splitter in config['prediction_splitters']])
+        if len(tdf) < 10:
+            print(f"{i}/{len(split_dfs)} | {atomic_id} | too short {len(tdf)}")
+        elif tdf['expense_plan_amount'].mean() < 1000 and tdf['expense_plan_amount'].max() < 10_000:
+            print(f"{i}/{len(split_dfs)} | {atomic_id} | too poor {tdf['expense_plan_amount'].mean():.0f}")
+        elif tdf.index[-1] < pd.to_datetime('2020-7-1'):
+            print(f"{i}/{len(split_dfs)} | {atomic_id} | ends too early len={len(tdf)} last_date={tdf.index[-1]}")
+        else:
+            model = AutoTS()
+            model.fit(tdf, series_column_name='expense_plan_amount', exogenous=['is_covid'])
 
     end_time = time()
     print(f'time to model: {end_time - start_time:.1f} seconds')
+
+
+def clean_data():
+    split_dfs = pkl.load(open('smol_split_dfs.pkl', 'rb'))
+    split_dfs = [test_df.set_index('finance_start_date', drop=False) for test_df in split_dfs]
+    split_dfs = add_zero_rows(split_dfs, start_date='2018-1-1', end_date='2020-7-1')
+
+    split_dfs = [is_covid(df) for df in split_dfs]
+
+    return split_dfs
+
+
+def add_zero_rows(split_dfs: list, start_date: str, end_date: str):
+    date_range = pd.date_range(pd.to_datetime(start_date), pd.to_datetime(end_date), freq='MS')
+    zero_df = pd.DataFrame({'zeros': [0] * len(date_range)}, index=date_range)
+
+    def add_zero_rows_helper(df: pd.DataFrame):
+        df = df.merge(zero_df, how='outer', left_index=True, right_index=True).drop('zeros', axis='columns')
+        df['expense_plan_amount'] = df['expense_plan_amount'].fillna(0)
+        df['finance_start_date'] = df['finance_start_date'].fillna(df.index.to_series())
+        df = df.fillna(method='backfill')
+        df = df.fillna(method='ffill')
+
+        return df
+
+    split_dfs = [add_zero_rows_helper(df) for df in split_dfs]
+
+    return split_dfs
+
+
+def is_covid(df: pd.DataFrame):
+    df['is_covid'] = 0
+    df.loc[df['finance_start_date'] > '2020-2-1', 'is_covid'] = 1
+    return df
 
 
 if __name__ == '__main__':
